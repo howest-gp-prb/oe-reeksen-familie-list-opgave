79a3028338496f163a3075c426a345c3c11bd7da
Dries Deboosere <dries.deboosere@howest.be>
2019-10-13 21:07:07 +0200

    Add project files.

---
+++ FamilieLists.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace FamilieLists.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
